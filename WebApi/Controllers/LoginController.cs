e84b0efc3f423954371c707f84d5189ee9f77d28
SMTSOFTWARE\mateusz.malinowski <mateusz.malinowski@intive.com>
2019-01-29 16:09:41 +0100

    Add Token config to Startup and logincontroler

--- WebApi/Controllers/LoginController.cs
+++ WebApi/Controllers/LoginController.cs
@@ -1,3 +1,7 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using Domain.AppUser.Model;
 using Domain.AppUser.Service;
@@ -5,6 +9,7 @@ using Entity;
 using Entity.Context;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 
 namespace WebApi.Controllers
 
@@ -23,15 +28,33 @@ namespace WebApi.Controllers
             var isCorrect = await _userManager.CheckPasswordAsync(applicationUser, aplicationUserModel.Password);
             if (isCorrect)
             {
-                return new ObjectResult("token");
+                var claims = new[]{
+                    new Claim(JwtRegisteredClaimNames.Sub,applicationUser.UserName),
+                    new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString())
+                };
 
+                var signingKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("SuperSecureKeyKey"));
+                var token = new JwtSecurityToken(
+                    issuer: "http://oec.com",
+                    audience: "http://oec.com",
+                    expires: DateTime.UtcNow.AddHours(1),
+                    claims: claims,
+                    signingCredentials: new Microsoft.IdentityModel.Tokens.SigningCredentials(signingKey, SecurityAlgorithms.HmacSha256)
+                );
+                return new OkObjectResult(
+                    new
+                    {
+                        token = new JwtSecurityTokenHandler().WriteToken(token),
+                        expiration = token.ValidTo
+                    }
+                );
             }
             else
             {
-                return new ObjectResult(applicationUser.PasswordHash);
+                return new UnauthorizedObjectResult("Unauthorized");
 
             }
-            // return new UnauthorizedObjectResult("Unauthorized");
+
         }
 
     }
